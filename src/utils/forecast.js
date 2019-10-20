d0b33262104d5b8dbdce965d950ff2c848cea012
Tom Ben Aroya <tom.ben1311@gmail.com>
2019-10-20 14:23:50 +0300

    Add new data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longtitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!');
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability} chance of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability} chance of rain. The high is ${body.daily.data[0].temperatureHigh} and low is ${body.daily.data[0].temperatureLow}`);
         }
     });
 }
